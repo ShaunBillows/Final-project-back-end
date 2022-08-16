6690f904eeafd25b9e95b2554b4d50c64e26c54d
OtterlyFaeRae <Moonturtledsc@Gmail.com>
2022-08-16 16:26:25 +0100

    Added connection, as well as user model & stock model.

---
+++ src/stock/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const stockSchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+    symbol: {
+        type: String, 
+        required: true
+    },
+    value: {
+        type: Number,
+        required: true
+    }
+});
+
+const Stock = mongoose.model('stock', stockSchema);
+
+module.exports = {Stock, stockSchema};
