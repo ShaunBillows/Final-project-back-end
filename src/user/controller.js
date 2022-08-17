ae93df73c0e9c155f129b8b9041255989fdc9a9a
ShaunBillows <billows.shaun@gmail.com>
2022-08-17 17:59:23 +0100

    refactored addStock controller

--- src/user/controller.js
+++ src/user/controller.js
@@ -86,38 +86,7 @@ exports.deleteUser = async (req, res) => {
     };
 };
 
-// exports.addStock = async (req, res) => {
-//   // updates user info
-//   console.log("Request recieved by addStock.");
-//   const stocks = req.user.stocks
-//   const user = req.user.stocks 
-//   const newStock = req.body.addStock
-//   try {
-//     let result
-//     if (stocks.some( x => JSON.parse(x).stock === newStock.stock)) { 
-//         // 1. if the new stock is in stocks increment the quantity 
-//         const userStocks = user.map( el => JSON.parse(el))
-//         const userStock = userStocks.find( el => el.stock === newStock.stock )
-//         // update stock quantity
-//         userStock.quantity = userStock.quantity + newStock.quantity
-//         const updatedStocks = userStocks.map( el => el.stock === newStock.stock ? [ JSON.stringify(userStock)] : [JSON.stringify(el)] )
-//         result = await User.updateOne({ username: req.user.username }, { stocks: updatedStocks })
-//      } else { 
-//         // 2. otherwise add the stock to stocks
-//         console.log("Adding new stock");
-//         result = await User.updateOne({ username: req.user.username }, { $addToSet: { stocks : [JSON.stringify(req.body.addStock)] } })
-//         console.log(result);
-//      }
-//       res.status(200).send({ msg: "Request processed.", result });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({ err: error.message });
-//   }
-// }
-
-
 exports.addStock = async (req, res) => {
-    // updates user info
     console.log("Request recieved by addStock.");
     try {
   
@@ -137,7 +106,6 @@ exports.addStock = async (req, res) => {
         result = await User.updateOne({ username: req.user.username }, { stocks: updatedStocks })
         console.log(result)
         console.log(userStocks)
-
   
        } else { 
         // 2. otherwise add the stock to stocks
@@ -146,15 +114,10 @@ exports.addStock = async (req, res) => {
         console.log(result);
         console.log(userStocks)
        }
-  
-  
-      // console.log(object);
-      // if (result) {
-        // console.log(result)
-        res.status(200).send({ msg: "Request processed.", result });
-      // } else {
-      //   throw new Error("No user found.")
-      // }
+       if (!result) {
+           throw new Error("No user found.")
+       } 
+    res.status(200).send({ msg: "Request processed.", result });
     } catch (error) {
       console.log(error);
       res.status(500).send({ err: error.message });
