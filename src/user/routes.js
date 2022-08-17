1f382d2cbb59965a3daa448df42e12eccc90a66e
OtterlyFaeRae <Moonturtledsc@Gmail.com>
2022-08-17 14:36:05 +0100

    Controllers and routes

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
 const userRouter = Router();
-const { createUser } = require('./controller');
-const { hashPass } = require('../middleware');
+const { createUser, deleteUser, login, updateEmail, updatePassword, updateUsername, updateMoney,  } = require('./controller');
+const { hashPass, checkPass, checkToken } = require('../middleware');
 
-userRouter.post('/user', hashPass, createUser)
+userRouter.post('/user', hashPass, createUser);
+userRouter.delete('/user', checkPass, deleteUser);
+userRouter.post('/login', checkPass, login);
+userRouter.get('/login', checkToken, login);
+userRouter.patch('/update-email', checkPass, updateEmail);
+userRouter.patch('/update-pass', checkPass, hashPass, updatePassword);
+userRouter.patch('/update-user', checkPass, updateUsername);
+userRouter.patch('/update-money', checkToken, updateMoney);
 
 module.exports = userRouter;
