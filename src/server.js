0eb901c3431d2ffbbdab57475a4c4bda7c39171c
OtterlyFaeRae <Moonturtledsc@Gmail.com>
2022-08-18 11:37:51 +0100

    Improved login response

--- src/server.js
+++ src/server.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(cors())
 app.use(userRouter);
 
-app.listen(process.env.PORT || 5000, ()=>{
+app.listen(process.env.PORT || 5001, ()=>{
     console.log('listening on port 5001')
 })
