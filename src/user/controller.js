c6554b69afb8ca363c8dcb8aedff82f5b4f6b325
ShaunBillows <billows.shaun@gmail.com>
2022-09-14 21:19:38 +0100

    stock controllers put into one route

--- src/user/controller.js
+++ src/user/controller.js
@@ -101,75 +101,153 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
-exports.updateCash = async (req, res) => {
+exports.updateCash = async (req, res, next) => {
+  console.log("updatecash hit");
   try {
-    if (!req.body.newCash) {
+    if (!req.cost) {
       throw new Error("Missing New Cash Value.");
     }
-    const cash = to2dp(req.body.newCash);
+    const newCash =  req.user.cash - req.cost
+    req.user.cash = newCash
     const result = await User.updateOne(
       { username: req.user.username },
-      { cash: cash }
+      { cash: newCash }
     );
     if (!result) {
       throw new Error("An error occured whilst updating the user.");
     }
-    res.status(200).send({
-      msg: `User ${req.user.username} cash updated.`,
-      cashUpdated: cash,
-    });
+    // res.status(200).send({
+    //   msg: `User ${req.user.username} cash updated.`,
+    //   user: req.user,
+    //   cashUpdated: req.newCash,
+    // });
+    // test
+    // const final = await User.findOne({ username: req.user.username })
+    next()
+    // res.status(200).send({
+    //   result: req.user
+    // });
+
   } catch (error) {
     res.status(500).send({ msg: `At updateCash: ${error.message}` });
   }
 };
 
-exports.addStock = async (req, res) => {
+// exports.addStock = async (req, res, next) => {
+//   console.log("addstocks hit");
+//   try {
+//     if (
+//       !req.body.addStock.name &&
+//       !req.body.addStock.symbol &&
+//       !req.body.addStock.number &&
+//       !req.body.price &&
+//       !req.body.buy
+//     ) {
+//       throw new Error("Missing value/s: name, symbol & number are required.");
+//     }
+//     if (typeof req.body.addStock.number !== "number") {
+//       throw new Error("Stock quantity must be a number.");
+//     }
+//     // const stocks = req.user.stocks;
+//     let result;
+//     const newStock = req.body.addStock;
+//     if (req.user.stocks.some((x) => x.name === newStock.name)) {
+//       // 1. if the new stock is in stocks increase the quantity
+//       // const userStocks = req.user.stocks;
+//       const userStock = req.user.stocks.find((el) => el.name === newStock.name);
+//       userStock.number = to2dp(to2dp(userStock.number) + to2dp(newStock.number));
+//       let updatedStocks;
+//       if (userStock.number > 0) {
+//         // a. if the quantity is greater than 0, update stocks
+//         updatedStocks = req.user.stocks.map((el) =>
+//           el.name === newStock.name ? userStock : el
+//         );
+//       } else {
+//         // b. otherwise remove the stock
+//         updatedStocks = req.user.stocks.filter((el) => el !== userStock);
+//       }
+//       result = await User.updateOne(
+//         { username: req.user.username },
+//         { stocks: updatedStocks }
+//       );
+//       req.user.stocks = updatedStocks;
+//     } else {
+//       // 2. otherwise add the stock to stocks
+//       req.body.addStock.number = to2dp(req.body.addStock.number);
+//       result = await User.updateOne(
+//         { username: req.user.username },
+//         { $addToSet: { stocks: req.body.addStock } }
+//       );
+//       req.user.stocks.push(req.body.addStock);
+//     }
+//     if (!result) {
+//       throw new Error("Incorrect credentials.");
+//     }
+//     // res.status(200).send({ msg: "Request processed.", user: req.user });
+//     // console.log(`quantity ${req.user.quantity }`+ " price " + req.body.addStock.price, + " cash " + req.user.cash);
+//     console.log(to2dp(req.body.addStock.price));
+//     console.log(to2dp(newStock.number));
+//     console.log(req.user.cash);
+//     req.cost = to2dp(newStock.number) * to2dp(req.body.price)
+//     // console.log(to2dp(newStock.number) * to2dp(req.body.price));
+//     next()
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({ err: `Error at AddStock: ${error.message}` });
+//   }
+// };
+
+exports.addStock = async (req, res, next) => {
+  console.log("addstocks hit");
   try {
     if (
-      !req.body.addStock.name &&
-      !req.body.addStock.symbol &&
-      !req.body.addStock.number
+      !req.body.addStock.name ||
+      !req.body.addStock.symbol ||
+      !req.body.addStock.number ||
+      !req.body.addStock.price
     ) {
       throw new Error("Missing value/s: name, symbol & number are required.");
     }
-    if (typeof req.body.addStock.number !== "number") {
+    console.log(typeof req.body.addStock.number);
+    console.log(typeof req.body.addStock.price);
+    if (typeof req.body.addStock.number !== "number" || typeof req.body.addStock.price !== "number") {
       throw new Error("Stock quantity must be a number.");
     }
-    const stocks = req.user.stocks;
-    const newStock = req.body.addStock;
     let result;
-    if (stocks.some((x) => x.name === req.body.addStock.name)) {
-      // 1. if the new stock is in stocks increase the quantity
-      const userStocks = req.user.stocks;
-      const userStock = userStocks.find((el) => el.name === newStock.name);
-      userStock.number = to2dp(userStock.number + newStock.number);
-      let updatedStocks;
-      if (userStock.number > 0) {
-        // a. if the quantity is greater than 0, update stocks
-        updatedStocks = userStocks.map((el) =>
-          el.name === newStock.name ? userStock : el
-        );
-      } else {
-        // b. otherwise remove the stock
-        updatedStocks = userStocks.filter((el) => el !== userStock);
-      }
-      result = await User.updateOne(
-        { username: req.user.username },
-        { stocks: updatedStocks }
-      );
-      req.user.stocks = updatedStocks;
-    } else {
-      // 2. otherwise add the stock to stocks
+    const newStock = req.body.addStock;
+    if (!req.user.stocks.some((x) => x.name === newStock.name)) {
+      // 1. if the user doesn't own the stock, push to their stocks
+      req.body.addStock.number = to2dp(req.body.addStock.number);
+      req.user.stocks.push(req.body.addStock);
       result = await User.updateOne(
         { username: req.user.username },
         { $addToSet: { stocks: req.body.addStock } }
       );
-      req.user.stocks.push(req.body.addStock);
+    } else {
+      // 2. if the user owns the stock, update the quantity
+      const userStock = req.user.stocks.find((el) => el.name === newStock.name);
+      userStock.number = to2dp(to2dp(userStock.number) + to2dp(newStock.number));
+      let updatedStocks;
+      if (userStock.number > 0) {
+        // 2i. if the quantity is greater than 0, update their stocks
+        updatedStocks = req.user.stocks.map((el) =>
+          el.name === newStock.name ? userStock : el
+        );
+      } else {
+        // 2ii. otherwise delete the stock
+        updatedStocks = req.user.stocks.filter((el) => el !== userStock);
+      }
+      req.user.stocks = updatedStocks;
+      result = await User.updateOne(
+        { username: req.user.username },
+        { stocks: updatedStocks }
+      );
     }
     if (!result) {
       throw new Error("Incorrect credentials.");
     }
-    res.status(200).send({ msg: "Request processed.", user: req.user });
+    req.cost = to2dp(newStock.number) * to2dp(req.body.addStock.price)
+    next()
   } catch (error) {
     console.log(error);
     res.status(500).send({ err: `Error at AddStock: ${error.message}` });
@@ -177,26 +255,28 @@ exports.addStock = async (req, res) => {
 };
 
 exports.addHistory = async (req, res) => {
+  console.log("addhistory hit");
   try {
-    if (
-      !req.body.symbol ||
-      !req.body.number ||
-      !req.body.price ||
-      typeof req.body.buy !== "boolean"
-    ) {
-      throw new Error("Missing field/s.");
-    }
+    // if (
+    //   !req.body.symbol ||
+    //   !req.body.number ||
+    //   !req.body.price ||
+    //   typeof req.body.buy !== "boolean"
+    // ) {
+    //   throw new Error("Missing field/s.");
+    // }
     const d = new Date();
     const transaction = {
-      symbol: req.body.symbol,
-      price: to2dp(req.body.price),
-      quantity: to2dp(req.body.number),
-      total: to2dp(req.body.number * req.body.price),
-      buy: req.body.buy,
+      symbol: req.body.addStock.name,
+      price: to2dp(req.body.addStock.price),
+      quantity: to2dp(Math.abs(req.body.addStock.number)),
+      total: to2dp(Math.abs(req.body.addStock.number) * req.body.addStock.price),
+      buy: req.body.addStock.number > 0 ? "Buy" : "Sell",
       timeStamp: `${d.getDate()}/${
         d.getMonth() + 1
       }/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`,
     };
+    req.user.history.push(transaction)
     const result = await User.updateOne(
       { username: req.user.username },
       { $push: { history: transaction } }
@@ -205,8 +285,6 @@ exports.addHistory = async (req, res) => {
       throw new Error("Unable to append transaction.");
     }
     res.status(200).send({
-      msg: `History entry successfully added.`,
-      transaction: transaction,
       user: req.user,
     });
   } catch (error) {
