0e6f2dba0e9753c1af1de7ac030db87e0977511f
ShaunBillows <billows.shaun@gmail.com>
2022-09-15 16:24:33 +0100

    formatting

--- src/user/routes.js
+++ src/user/routes.js
@@ -16,8 +16,6 @@ userRouter.post("/login", checkPass, login);
 userRouter.post("/user", hashPass, createUser);
 userRouter.delete("/user", checkToken, deleteUser);
 userRouter.patch("/user", checkToken, checkPass, updateUser);
-// userRouter.patch("/user/cash", checkToken, updateCash);
 userRouter.patch("/user/stocks", checkToken, addStock, updateCash, addHistory);
-// userRouter.patch("/user/history", checkToken, addHistory);
 
 module.exports = userRouter;
